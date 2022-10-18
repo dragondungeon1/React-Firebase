2763771595f3bb9b092daf7ffaac61300c634fde
Lennart <lennartderidder@gmail.com>
2022-10-18 12:49:38 +0200

    feat: started with creating login with username and password + some styling and comments

--- components/message.js
+++ components/message.js
@@ -21,12 +21,14 @@ export default function Message({children, avatar, username, description}) {
             {children}
          </div>
         </div>
-    ) : (
+    ) :
+        //when out of edit area
+        (
         <div>
             {/*<div className="bg-white mt-10 p-8 border-b-2 rounded-lg shadow-lg">*/}
             {/*<div className="flex items-center gap-2">*/}
             {/*<img src={avatar} alt="" className="w-10 rounded-full"/>*/}
-            <h2>{username}</h2>
+            {/*<h2>{username}</h2>*/}
             {/*</div>*/}
             {/*<div className="py-4">*/}
             <p>{description}</p>
