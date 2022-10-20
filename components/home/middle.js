65028656668462af23edc8ca8e468828f9328e7f
Lennart <lennartderidder@gmail.com>
2022-10-20 20:55:37 +0200

    feat: added a service operation

--- components/home/middle.js
+++ components/home/middle.js
@@ -1,3 +1,7 @@
+import {useEffect, useState} from "react";
+import {collection, onSnapshot, orderBy, query} from "firebase/firestore";
+import {db} from "../../utils/firebase";
+
 const features = [
     { name: 'React', description: 'JavaScript framework build by facebook' },
     { name: 'Firebase', description: 'Firebase is a hosting service from google'},
@@ -6,6 +10,19 @@ const features = [
 ]
 
 export default function Middle() {
+    const [allServices, setAllServices] = useState([]);
+
+    const getServices = async () => {
+        const collectionRef = collection(db, 'services')
+        const q = query(collectionRef, orderBy('timestamp', 'desc'))
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            setAllServices(snapshot.docs.map((doc) => ({...doc.data(), id: doc.id})));
+            return unsubscribe;
+        });
+    };
+    useEffect(() => {
+        getServices();
+    }, []);
     return (
         <div className="bg-white">
             <div className="mx-auto grid lg:max-w-[85%] max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
@@ -17,10 +34,10 @@ export default function Middle() {
                     </p>
 
                     <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-                        {features.map((feature) => (
-                            <div key={feature.name} className="border-t border-gray-200 pt-4">
-                                <dt className="font-medium text-gray-900">{feature.name}</dt>
-                                <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
+                        {allServices.map((service) => (
+                            <div key={service.id} className="border-t border-gray-200 pt-4">
+                                <dt className="font-medium text-gray-900">{service.title}</dt>
+                                <dd className="mt-2 text-sm text-gray-500">{service.shortDescription}</dd>
                             </div>
                         ))}
                     </dl>
